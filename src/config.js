f5ee3c23aa1cccc0d7ac0c9afa28e079421ef1c0
Nel Rondón <71944026+nelrondon@users.noreply.github.com>
2025-07-17 03:01:18 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ export const { PORT = 3000, SALT_ROUNDS = 10, SECRET_KEY } = process.env;
 
 export const ALLOWED_ORIGINS = [
   "http://localhost:5173", // Frontend de desarrollo
-  "https://proyecto-estructuras-topaz.vercel.app/",
+  "https://proyecto-estructuras-topaz.vercel.app",
 ];
